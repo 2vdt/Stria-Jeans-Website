cf99ca55e9ae19af1d00c4da8224d1c23e10d3d2
RAMISH104 <131403671+2vdt@users.noreply.github.com>
2025-12-31 22:37:28 +0500

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import Navbar from './components/Navbar/Navbar';
+import Hero  from './components/Hero/Hero';
+import AOS from "aos";
+import "aos/dist/aos.css";
+import Poppup from './components/Poppup/Poppup';
+import Banner from './components/Banner/Banner';
+import Products from './components/Products/Products';
+import TopProducts from './components/TopProducts/TopProducts';
+import Subscribe from './components/Subscribe/Subscribe';
+import Testimonials from './components/TopProducts/Testimonials/Testimonials';
+import Footer from './components/TopProducts/Testimonials/Footer/Footer';
+const App = () => {
+  const [orderPopup, setOrderPopup] = React.useState(false);
+
+  const handleOrderPopup = () => {
+    setOrderPopup(!orderPopup);
+  };
+  React.useEffect(() => {
+    AOS.init({
+      offset: 100,
+      duration: 800,
+      easing: "ease-in-sine",
+      delay: 100,
+    });
+    AOS.refresh();
+  }, []);
+  return (
+    <div className="bg-white dark:bg-gray-900 dark:text-white duration-200">
+      <Navbar handleOrderPopup={handleOrderPopup}/>
+      <Hero handleOrderPopup={handleOrderPopup}/>
+      <Products/>
+      <TopProducts handleOrderPopup={handleOrderPopup} />
+      <Banner/>
+      
+      <Subscribe/>
+      <Products/>
+      <Testimonials/>
+      <Footer/>
+       <Poppup orderPopup={orderPopup} setOrderPopup={setOrderPopup} />
+    </div>
+  );
+};
+
+export default App;
